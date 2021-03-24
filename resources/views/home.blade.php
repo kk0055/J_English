c046cb1a31eca8b26bf1d2985d9ea88cdbd973e5
kk0055 <rtsw.lgy@gmail.com>
2021-03-24 13:52:43 +0800

    ルール累加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     
 
         <!--Section container-->
-        <section class="w-full ">
+        <div class="w-full mb-10 ">
 
             <!--Title-->
             <h1 class="font-sans font-bold break-normal text-gray-700 px-2 text-xl mt-5  md:text-2xl">
@@ -19,22 +19,24 @@
             <h2 id='section1' class="font-sans font-bold break-normal text-gray-700 px-2 pb-3 text-xl">ルール</h2>
             <a  class="inline-flex  justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 bg-black" href="{{ route('create') }}">問題をつくる</a>
         </div>
-            <p class="ml-3">日本語を見て5秒以内に英語を声に出す。<br> 5秒考えてわからなければ日本語をクリックして英語を見る。</p>
+            <p class="ml-3">日本語を見て5秒以内に英語を声に出す。<br> 5秒考えてわからなければ日本語をクリックして英語を見る。
+            <br>ログインすれば問題の追加と問題の保存ができる。
+            <br>問題はシャッフルだよー。<span class="text-blue-700 font-bold">声出してねー</span></p>
+            
 
             <!--Card-->
             @foreach ($languages as  $language)
             <div class="p-5 mt-6 lg:mt-0 leading-normal rounded shadow bg-white">
             
-             
-              <button class="question focus:outline-none"> {{ $language->japanese }} </button>
+            <button class="question focus:outline-none">{{ $language->japanese }} </button>
               <p class="english mt-2 text-red-500" >{{ $language->english }}
                </p> 
             </div>
             @endforeach
-            <!--/Card-->
-      <!--/container-->
+       
+
       {{-- {{ $languages->links() }} --}}
-    </section >
+    </div >
 
     <script>
 
