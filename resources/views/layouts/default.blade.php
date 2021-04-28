2c14a286c0137ff996a335c73a485179ed43cc89
kk0055 <rtsw.lgy@gmail.com>
2021-04-28 11:59:37 +0800

    meta 修正

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,11 +10,16 @@
     <meta property="og:title" content="5秒英作文 ～5seconds English～"/>
     <meta name="keywords" content="瞬間英作文,スピーキング,英語,5秒英作文,TOEIC,英会話,英語学習">
     <meta name="description" content="5秒英作文。日本語を見て5秒以内に英語を声に出します。基本的な文法を使って、比較的短い日本語を5秒で英作文するトレーニングを進めていきます。短時間でもいいので、毎日繰り返し練習して英語になれましょう。">
-    <meta property="st:section" content="秒英作文。日本語を見て5秒以内に英語を声に出します。基本的な文法を使って、比較的短い日本語を5秒で英作文するトレーニングを進めていきます。短時間でもいいので、毎日繰り返し練習して英語になれましょう。"/>
+    <meta property="st:section" content="5秒英作文。日本語を見て5秒以内に英語を声に出します。基本的な文法を使って、比較的短い日本語を5秒で英作文するトレーニングを進めていきます。短時間でもいいので、毎日繰り返し練習して英語になれましょう。"/>
     <meta name="twitter:title" content="5秒英作文 ～5seconds English～"/>
     <meta property="og:url" content="https://5seconds-en.com/" />
     <meta property="og:image" content="https://image.freepik.com/free-photo/top-view-decoration-with-cubes-wooden-background_23-2148389005.jpg" />
-   
+    {{-- Twitter --}}
+    <meta name="twitter:card" content="Summary">
+    <meta name="twitter:title" content="5秒英作文 ～5seconds English～">
+    <meta name="twitter:description" content="4.5秒英作文。日本語を見て5秒以内に英語を声に出します。基本的な文法を使って、比較的短い日本語を5秒で英作文するトレーニングを進めていきます。短時間でもいいので、毎日繰り返し練習して英語になれましょう。">
+    <meta name="twitter:image" content="https://image.freepik.com/free-photo/top-view-decoration-with-cubes-wooden-background_23-2148389005.jpg" >
+
     {{-- Search Console --}}
     <meta name="google-site-verification" content="Xja4SEPoY8ersK2goh_5cGHoLdvCDalwlmQLTV1F-io" />
 
