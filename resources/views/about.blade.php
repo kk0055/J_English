5baac9190b83b8d53c2b6d48be12e101484774dc
kk0055 <rtsw.lgy@gmail.com>
2021-05-15 13:22:01 +0800

    text size 修正

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -63,7 +63,7 @@
 	<!--Container-->
 	<div class="container w-full md:max-w-3xl mx-auto pt-20">
 
-		<div class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
+		<div class="w-full px-4 md:px-6 text-sm md:text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
 
 			<!--Title-->
 			<div class="font-sans">
