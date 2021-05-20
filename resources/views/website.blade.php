db785e0328fa4d042c789eb2e721e35ed94d5ec0
kk0055 <rtsw.lgy@gmail.com>
2021-05-21 00:53:23 +0800

    font 修正

--- resources/views/website.blade.php
+++ resources/views/website.blade.php
@@ -5,7 +5,7 @@
         <div class="w-full mb-10  bg-white">
             @include('nav')
 
-            <h1 class="font-sans font-bold break-normal text-gray-700 px-2 text-2xl mt-5  md:text-2xl">
+            <h1 class="font-bold break-normal  px-2 text-2xl mt-5  md:text-2xl">
               英語学習用サイトの紹介<i class="fas fa-paw ml-1"></i>
 		      	</h1>
         <p class="px-3 mt-3">英語学習に役立つYoutube動画やサイトを紹介します。</p>
@@ -16,23 +16,23 @@
         </div>
 <div class="websiteList" >
 <ul >
-    <li class="font-sans font-bold break-normal text-gray-700 mt-3 pb-3 text-xl">Udemy</li>
+    <li class=" font-bold break-normal  mt-3 pb-3 text-xl">Udemy</li>
     <a class="text-blue-500 hover:text-blue-700" href="https://www.udemy.com/courses/search/?price=price-free&q=english&sort=relevance&src=ukw" target=_blank>Udemyにジャンプ</a>
 
     <p class="mt-3">Udemyの無料で利用できる英語の講座です。<br> 
     現在864個の講座が無料で受けられます。Udemyにジャンプしたら自分のアカウントの言語を英語に変えてください。<br>
      英語で英語学習ができます。 自分の目的に合わせてつかってみてください。</p>
      {{-- block --}}  
-     <li class="font-sans font-bold break-normal text-gray-700 mt-5 pb-3 text-xl ">英単語アプリ mikan</li>
+     <li class=" font-bold break-normal  mt-5 pb-3 text-xl ">英単語アプリ mikan</li>
      <a class="text-blue-500 hover:text-blue-700" href="https://apps.apple.com/jp/app/%E8%8B%B1%E5%8D%98%E8%AA%9E%E3%82%A2%E3%83%97%E3%83%AA-mikan/id920856839" target=_blank>英単語アプリ mikan</a>
  
      {{-- block --}}
-     <li class="font-sans font-bold break-normal text-gray-700 mt-5 pb-3 text-xl ">English Listening Lesson Library Online</li>
+     <li class=" font-bold break-normal  mt-5 pb-3 text-xl ">English Listening Lesson Library Online</li>
      <a class="text-blue-500 hover:text-blue-700" href="https://www.elllo.org/index.htm" target=_blank>English Listening Lesson Library Online</a>
  
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">Learn English While Sleeping</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">Learn English While Sleeping</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/OazE2vXzLf4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -41,7 +41,7 @@
     {{-- block end --}}
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">English Conversation; Learn while you Sleep with 5000 words</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">English Conversation; Learn while you Sleep with 5000 words</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/3ha-kqV0P38" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -50,7 +50,7 @@
     {{-- block end --}}
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">74 Topics - Daily Life English conversations</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">74 Topics - Daily Life English conversations</li>
     <div class="iframe-wrap">
     <iframe width="560"  height="315" src="https://www.youtube.com/embed/bAPpaP5exR0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -59,7 +59,7 @@
 
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">Daily English Conversation</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">Daily English Conversation</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/RCseemNc2zw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
      </div>
@@ -67,7 +67,7 @@
     {{-- block end --}}
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">American English Passages for Reading and Listening With an American Accent</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">American English Passages for Reading and Listening With an American Accent</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/3w4RnhjNDkQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
      </div>
@@ -75,7 +75,7 @@
     {{-- block end --}}
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">58 Passages from Elementary to Advanced Level</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">58 Passages from Elementary to Advanced Level</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/mIbAPEPbptY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -83,7 +83,7 @@
     {{-- block end --}}   
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">All about America English Listening Passages With Native English Speakers</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">All about America English Listening Passages With Native English Speakers</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/_QhcGOSA4z0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
      </div>      
@@ -91,7 +91,7 @@
     {{-- block end --}}  
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">Academic American English - Listening and Reading</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">Academic American English - Listening and Reading</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/MQkXa-gHSuc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
      </div>
@@ -100,7 +100,7 @@
 
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">Daily English Conversation Practice Questions and Answers - Improve Vocabulary - Sleep Learning</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">Daily English Conversation Practice Questions and Answers - Improve Vocabulary - Sleep Learning</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/Lw3p7rgOVaA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -108,7 +108,7 @@
     {{-- block end --}}  
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">English Conversation & Listening Practice - English Practice Listening to Naturally</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">English Conversation & Listening Practice - English Practice Listening to Naturally</li>
     <div class="iframe-wrap">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/9m_MxHuK_-U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -116,7 +116,7 @@
     {{-- block end --}}  
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">American English Conversations to Improve Listening & Speaking Fluency | English Conversation</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">American English Conversations to Improve Listening & Speaking Fluency | English Conversation</li>
     <div class="iframe-wrap">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/v-Htpt-ZtcA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -124,7 +124,7 @@
     {{-- block end --}}  
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">1000 COMMON ENGLISH QUESTIONS AND ANSWERS for beginners | English Conversation</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">1000 COMMON ENGLISH QUESTIONS AND ANSWERS for beginners | English Conversation</li>
     <div class="iframe-wrap">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/p6EQKQK8Z1M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -132,7 +132,7 @@
     {{-- block end --}}     
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">50 Daily English Conversations </li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">50 Daily English Conversations </li>
     <div class="iframe-wrap">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/kxb8EpyhKro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -140,7 +140,7 @@
     {{-- block end --}} 
 
         {{-- block --}}
-        <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">Real English Conversations - Short Dialogs </li>
+        <li class=" font-bold break-normal  mt-20 pb-3 text-xl">Real English Conversations - Short Dialogs </li>
         <div class="iframe-wrap">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/GUUNahFODcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
@@ -148,7 +148,7 @@
         {{-- block end --}} 
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">Friends</li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">Friends</li>
     <div class="iframe-wrap">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/A9GyVeV1Yu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -156,7 +156,7 @@
     {{-- block end --}}     
 
     {{-- block --}}
-    <li class="font-sans font-bold break-normal text-gray-700 mt-20 pb-3 text-xl">TOEFL Listening Practice </li>
+    <li class=" font-bold break-normal  mt-20 pb-3 text-xl">TOEFL Listening Practice </li>
     <div class="iframe-wrap">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Dahw9ZxlNFo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
