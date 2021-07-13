8e67cb8ab5bdfd9e02f76aa56d4e0c591ef9d736
kk0055 <rtsw.lgy@gmail.com>
2021-07-13 22:42:26 +0800

    admin検索画面修正

--- resources/views/admin/search_results.blade.php
+++ resources/views/admin/search_results.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.default')
 
 @section('content')
-    @include('search_results')
+    
+<div class="w-full mb-10 lg:w-4/5 lg:mx-auto">
+    <div class="my-3">
+        <a href="{{ url('admin/user_list') }}" class="inline-flex mb-3 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 bg-black">ユーザー 一覧</a>
+    </div>
+
+    @if (session('status'))
+    <div class="ml-5 text-green-500 text-2xl font-bold" role="alert">
+    {{ session('status') }}
+    </div>
+    @endif
+
+    <h1 class="font-bold break-normal text-gray-700 px-2 text-2xl mt-5  md:text-2xl">
+    管理画面用検索結果
+   
+    </h1>
+
+    <hr class="bg-gray-300 my-12">
+    @include('components.search_result')
+ 
+ </div>
+
 @endsection
---
+++ resources/views/components/search_result.blade.php
@@ -0,0 +1,36 @@
+   <!--Title-->
+   <div class="flex justify-between ">
+    <h2 id='section1' class="font-bold break-normal text-gray-700 px-2 pb-3 text-xl">検索結果 <i class="far fa-cat"></i></h2>
+  
+    </div>
+
+        {{-- Google Ads --}}
+        @guest
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- ベーシック１ -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-6877145874439358"
+             data-ad-slot="5867733062"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+         <br>
+         @endguest
+        {{-- !Google Ads --}}
+        
+    <div class="ml-2 mt-3 ">
+    @if (!$languages->count())
+    <p>検索したワードはまだ無いみたいだね！<br> 君が作成してみては！？
+   
+   </p>
+   <a  class="inline-flex mt-5  justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 bg-black" href="{{ route('create') }}">問題をつくる</a>
+  </div>
+
+   @else
+   <div class="">
+    @include('components.main')
+  </div>
+    @endif
