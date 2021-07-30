0512b0c6a850194535b49239ac3570f8061c465b
kk0055 <rtsw.lgy@gmail.com>
2021-07-30 10:40:31 +0800

    ガチャ ナブを作成

--- resources/views/components/gacha_nav.blade.php
+++ resources/views/components/gacha_nav.blade.php
@@ -10,52 +10,11 @@
       <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
           <div class="nav">
               <ul class="md:flex items-center justify-between text-base text-gray-600 pt-4 md:pt-0">
-                <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('about') }}">はじめに</a></li>
                 <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('englishWord') }}">ガチャガチャ英単語</a></li>
-                <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('website') }}">英語学習用サイト</a></li>
-              
-                @guest
-                  <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('login') }}">ログイン</a></li>
-                  <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('register') }}">登録</a></li>
-                @endguest
-                @auth
-                  <li>
-                    <a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('user.post',Auth::user()) }}">{{ Auth::user()->name }}の投稿</a>
-                 </li>
-             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-             <li>
-                <a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('my_favorites') }}">お気に入り(保存)</a>
-            </li>
-         
-            <a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-           document.getElementById('logout-form').submit();">
-             ログアウト
-         </a></li>
-             @endauth
-           
+                <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="/">5秒英作文</a></li>
+          
               </ul>    
           </div>
       </div>
-             <div class="order-1 md:order-2  ">
-                  {{-- Routeがjapaneseの時だけ--}}
-                  @if (Request::is('japanese'))
-                  <div class="flex">
-                  <a class="font-bold text-xl mr-5" href="japanese">
-                    <i class="fas fa-redo"></i>
-                  </a>  
-                  <a class="md:mr-10 font-bold text-xl"  href="/">
-                    <i class="fas fa-home"  ></i>
-                  </a>   
-                  {{-- Routeがjapaneseの時だけ--}}
-                </div>
-                  @else
-                  <a class=" md:mr-10 font-bold text-xl"  href="/">
-                    <i class="fas fa-home"></i>
-                  </a> 
-                  @endif
-            </div>
   </div>
 </nav>
