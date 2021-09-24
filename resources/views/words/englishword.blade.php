fde4232082ed7c32c415bd4c7ba9e349a6d8395b
kk0055 <rtsw.lgy@gmail.com>
2021-09-24 22:08:45 +0800

    Teet button修正

--- resources/views/words/englishword.blade.php
+++ resources/views/words/englishword.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 @include('components.gacha_nav')    
-<div class="lg:ml-64 ml-4 mt-3">
+<div class="lg:ml-64 ml-5 mt-3">
     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
+</div>
     <english-word/>
  
 @endsection
