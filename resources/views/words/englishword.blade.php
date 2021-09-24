a5dc2e492ecd16f62ace17c62a20ca4f24890b4f
kk0055 <rtsw.lgy@gmail.com>
2021-09-24 22:06:20 +0800

    Teet button修正

--- resources/views/words/englishword.blade.php
+++ resources/views/words/englishword.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @include('components.gacha_nav')    
-<div class="ml-64 mt-3">
+<div class="lg:ml-64 ml-4 mt-3">
     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
     <english-word/>
--- resources/views/words/japaneseword.blade.php
+++ resources/views/words/japaneseword.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @include('components.gacha_ja_nav')
 
-<div class="ml-64 mt-3">
+<div class="lg:ml-64 ml-4 mt-3">
 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
     <japanese-word/>
